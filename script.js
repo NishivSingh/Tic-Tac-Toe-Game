241384190b197dc28539ef2522156120089650ec
Nishiv Singh <nishiv2001@gmail.com>
2022-07-22 15:45:28 +0530

    Initial Files

---
+++ script.js
@@ -0,0 +1,65 @@
+let tick = new Audio("music/ting.mp3");
+let music = new Audio("music/music.mp3");
+let gameover = new Audio("music/gameover.mp3");
+let turn = "X";
+let won = false;
+
+// Function to chnage the turn 
+const changeturn = () =>{
+    return turn == "X"?"0":"X";
+}
+
+// Function to check for a win 
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0,1,2,0,5,0],
+        [3,4,5,0,15,0],
+        [6,7,8,0,25,0],
+        [0,3,6,-10,15,90],
+        [1,4,7,0,15,90],
+        [2,5,8,10,15,90],
+        [0,4,8,0,15,45],
+        [2,4,6,0,15,-45],
+    ]
+    wins.forEach(e=>{
+        if ((boxtext[e[0]].innerText == boxtext[e[1]].innerText) && (boxtext[e[1]].innerText == boxtext[e[2]].innerText) && boxtext[e[0]].innerText != ""){
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won !";
+            won = true;
+            document.querySelector(".line").style.width = "30vw";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            document.querySelector('.image').getElementsByTagName('img')[0].style.width = "150px";
+            gameover.play();
+        }
+    })
+}
+// Game logic
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(e=>{
+    let boxtext = e.querySelector('.boxtext');
+    e.addEventListener('click',()=>{
+        if (boxtext.innerText == ''){
+            boxtext.innerText = turn;
+            turn = changeturn();
+            tick.play();
+            checkWin();
+            if (!won){
+                document.getElementsByClassName("info")[0].innerText = "Turn for "+turn;
+            }
+        }
+    })
+})
+
+// Reset 
+// let reset = document.getElementsByClassName("reset");
+reset.addEventListener('click',()=>{
+    let boxtext = document.querySelectorAll('.boxtext');
+    Array.from(boxtext).forEach(ele=>{
+        ele.innerText = "";
+    });
+    turn = "X";
+    won = false;
+    document.querySelector(".line").style.width="0";
+    document.querySelector(".image img").style.width = "0";
+    document.getElementsByClassName("info")[0].innerText = "Turn for "+turn;
+})
